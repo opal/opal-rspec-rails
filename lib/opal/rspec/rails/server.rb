d8c0fc51a4fc86c0c078247da8f7e656e824ba9a
Elia Schito <elia@schito.me>
2016-01-02 00:34:58 +0100

    Remove debug output

--- lib/opal/rspec/rails/server.rb
+++ lib/opal/rspec/rails/server.rb
@@ -7,7 +7,7 @@ class Opal::RSpec::Rails::Server < ::Opal::Server
     app    = Rails.application
     assets = app.config.opal_rspec.sprockets_instance || app.assets
     assets.append_path Opal::RSpec::Rails.root.join('opal').to_s
-    assets.append_path p Rails.root.join(app.config.opal_rspec.spec_location).to_s
+    assets.append_path Rails.root.join(app.config.opal_rspec.spec_location).to_s
 
     super(options.merge(sprockets: assets))
   end
